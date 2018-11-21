09fcdd473dae3c4e528810ef8dcf0c64e4ccf422
Haruto Hirakawa <31243896+JPNYKW@users.noreply.github.com>
2018-11-22 00:23:36 +0900

    Add files via upload

---
+++ js/canvas.js
@@ -0,0 +1,299 @@
+const Renderer = CanvasRenderingContext2D;
+
+Renderer.prototype.shape = function (status) {
+    if (status.v < 3) {
+        return false;
+    }
+
+    let color = status.color || '#e3e3e1';
+    let bold = status.bold || null;
+
+    this.beginPath();
+
+    if (bold == null) {
+        this.fillStyle = color;
+    } else {
+        this.strokeStyle = color;
+        this.lineWidth = bold;
+    }
+
+    let x = status.x || 0;
+    let y = status.y || 0;
+
+    let d = status.d || status.direction || 0;
+    let r = status.r || status.radius || 32;
+
+    let v = status.v || status.vertex || 3;
+
+    let zeroVertex = new Array(v).fill(0);
+    let fv = status.fv || zeroVertex;
+    let fx = status.fx || zeroVertex;
+    let fy = status.fy || zeroVertex;
+
+    let dis_theta = 360 / v;
+    let theta = d;
+
+    for (let i = 0; i < v; i ++) {
+        let point = {
+            x: x + Math.cos(theta.toRadian()) * (r + fv[i]) + fx[i],
+            y: y + Math.sin(theta.toRadian()) * (r + fv[i]) + fy[i]
+        };
+
+        if (i == 0) {
+            this.moveTo(point.x, point.y);
+        } else {
+            this.lineTo(point.x, point.y);
+        }
+
+        theta += dis_theta;
+    }
+
+    this.closePath();
+
+    if (bold == null) {
+        this.fill();
+    } else {
+        this.stroke();
+    }
+}
+
+Renderer.prototype.circle = function (status) {
+    let x = status.x || 0;
+    let y = status.y || 0;
+    let r = status.r || status.radius || 32;
+
+    let bold = status.bold || null;
+    let color = status.color || '#e3e3e1';
+
+    this.beginPath();
+
+    if (bold == null) {
+        this.fillStyle = color;
+    } else {
+        this.strokeStyle = color;
+        this.lineWidth = bold;
+    }
+
+    this.arc(x, y, r, 0, Math.PI * 2);
+    this.closePath();
+
+    if (bold == null) {
+        this.fill();
+    } else {
+        this.stroke();
+    }
+}
+
+Renderer.prototype.text = function (status) {
+    let x = status.x || 0;
+    let y = status.y || 0;
+
+    let bold = status.bold || null;
+    let color = status.color || '#e3e3e1';
+
+    let align = status.align || 'center';
+    let text = status.text || 'NO TEXT DATA';
+
+    let px = status.px || 16;
+    let font = status.font || 'Arial';
+
+    this.beginPath();
+    this.textAlign = align;
+    this.font = `${px}px ${font}`;
+
+    if (bold == null) {
+        this.fillStyle = color;
+        this.fillText(text, x, y);
+    } else {
+        this.strokeStyle = color;
+        this.strokeText(text, x, y);
+    }
+}
+
+Renderer.prototype.line = function (status) {
+    let color = status.color || '#e3e3e1';
+    let bold = status.bold || 1;
+
+    let x = status.x || 0;
+    let y = status.y || 0;
+    let x2 = status.x2 || 0;
+    let y2 = status.y2 || 0;
+
+    this.beginPath();
+    this.lineWidth = bold;
+    this.strokeStyle = color;
+
+    this.moveTo(x, y);
+    this.lineTo(x2, y2);
+
+    this.closePath();
+    this.stroke();
+}
+
+Renderer.prototype.negative = function (status) {
+    let x = status.x || 0;
+    let y = status.y || 0;
+
+    let width = status.w || status.width || 32;
+    let height = status.h || status.height || 32;
+
+    let image = this.getImageData(x, y, width, height);
+    let data = image.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        data[i] = 255 - data[i];
+        data[i + 1] = 255 - data[i + 1];
+        data[i + 2] = 255 - data[i + 2];
+    }
+
+    this.putImageData(image, x, y);
+}
+
+Renderer.prototype.noise = function (status) {
+    let x = status.x || 0;
+    let y = status.y || 0;
+
+    let width = status.w || status.width || 32;
+    let height = status.h || status.height || 32;
+
+    let gray = status.gray || false;
+    let level = status.level || 1;
+
+    let alpha = status.alpha || 0;
+
+    let image = this.getImageData(x, y, width, height);
+    let data = image.data;
+
+    if (gray) {
+        for (let i = 0; i < data.length; i += 4) {
+            let color = Math.random() * (level * 2) - level;
+
+            data[i] += color;
+            data[i + 1] += color;
+            data[i + 2] += color;
+
+            data[i + 3] = alpha || Math.random() * 255;
+        }
+    } else {
+        for (let i = 0; i < data.length; i += 4) {
+            data[i] += Math.random() * (level * 2) - level;
+            data[i + 1] += Math.random() * (level * 2) - level;
+            data[i + 2] += Math.random() * (level * 2) - level;
+
+            data[i + 3] = alpha || Math.random() * 255;
+        }
+    }
+
+    this.putImageData(image, x, y);
+}
+
+Renderer.prototype.glitch = function (status) {
+    let x = status.x || 0;
+    let y = status.y || 0;
+
+    let width = status.w || status.width || 32;
+    let height = status.h || status.height || 32;
+
+    let level = status.level || 1;
+
+    let image = this.getImageData(x, y, width, height);
+    let data = image.data;
+
+    switch (status.type) {
+        default:
+            let redFix = (Math.random() * level) >> 0;
+            let greenFix = (Math.random() * level) >> 0 * level;
+            let blueFix = (Math.random() * level) >> 0;
+
+            for (let i = 0; i < data.length; i += 4) {
+                data[i] = data[i + redFix * 4];
+                data[i + 1] = data[i + 1 + greenFix * 4];
+                data[i + 2] = data[i + 2 + blueFix * 4];
+
+                data[i + 3] = 180;
+            }
+            break;
+
+        case 'box':
+            let max = width * height;
+            let mix = this.mix || false;
+
+            for (let i = 0; i < level; i ++) {
+                let i = ((Math.random() * max) >> 0) * 4;
+
+                for (let j = 0, l = (Math.random() * 8000) >> 0; j < l; j ++) {
+                    if (mix && Math.random() < 0.5) {
+                        [data[i], data[i - height * 4]] = [data[i - height * 4], data[i]];
+                        [data[i + 1], data[i + 1 - height * 4]] = [data[i + 1 - height * 4], data[i + 1]];
+                        [data[i + 2], data[i + 2 - height * 4]] = [data[i + 2 - height * 4], data[i + 2]];
+                    } else {
+                        [data[i], data[i + height * 4]] = [data[i + height * 4], data[i]];
+                        [data[i + 1], data[i + 1 + height * 4]] = [data[i + 1 + height * 4], data[i + 1]];
+                        [data[i + 2], data[i + 2 + height * 4]] = [data[i + 2 + height * 4], data[i + 2]];
+                    }
+
+                    i += 4;
+                }
+            }
+            break;
+
+        case 'line':
+            let w = image.width;
+            let h = image.height;
+
+            let yPos = new Array(h).fill(0).map((x, y) => y);
+            let ids = [];
+
+            for (let i = 0; i < level; i ++) {
+                ids.push(yPos.random(true));
+            }
+
+            ids.map(id => {
+                let dy = -level + Math.random() * (level * 2);
+
+                for (let i = id * w;; j < w; j ++) {
+                    let k = i * 4;
+                    [data[k], data[k + dy * w]] = [data[k + dy * w], data[i]];
+                    [data[k], data[k + 1 + dy * w]] = [data[k + 1 + dy * w], data[i]];
+                    [data[k], data[k + 2 + dy * w]] = [data[k + 2 + dy * w], data[i]];
+                }
+            });
+            break;
+    }
+
+    this.putImageData(image, x, y);
+}
+
+Renderer.prototype.lightness = function (level, canvas) {
+    let height = canvas.height || 255;
+    let width = canvas.width || 255;
+
+    let image = this.getImageData(0, 0, width, height);
+    let data = image.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        data[i] += level;
+        data[i + 1] += level;
+        data[i + 2] += level;
+    }
+
+    this.putImageData(image, 0, 0);
+}
+
+Renderer.prototype.gray = function (canvas) {
+    let height = canvas.height;
+    let width = canvas.width;
+
+    let image = this.getImageData(0, 0, width, height);
+    let data = image.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        let color = (data[i] + data[i + 1] + data[i + 2]) / 3;
+
+        data[i] = color;
+        data[i + 1] = color;
+        data[i + 2] = color;
+    }
+
+    this.putImageData(image, 0, 0);
+}
---
+++ js/class.js
@@ -0,0 +1,218 @@
+class Character {
+    constructor (status) {
+        Object.assign(this, status);
+        this.disappear = -1;
+        this.tick = 0;
+    }
+
+    move (dx, dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    checkOverArea () {
+        let disappear = this.disappear;
+
+        let r = this.r || 10;
+        let max = 512 + r;
+        let min = -r;
+
+        switch (disappear) {
+            case -1:
+                disappear += (min < this.x && max > this.x && min < this.y && max > this.y);
+                break;
+
+            case 0:
+                disappear += !(min < this.x && max > this.x && min < this.y && max > this.y) * 2;
+                break;
+        }
+
+        this.disappear = disappear;
+    }
+}
+
+class Player extends Character {
+    constructor (status) {
+        super(status);
+    }
+
+    draw (context) {
+        let size = this.size;
+        context.shape({x: this.x, y: this.y + 10, d: 270, r: size, v: 3, bold: 1.3, color: '#2e2e2e', fv: [size / 1.5, size / -3, size / -3]});
+        context.shape({x: this.x, y: this.y + 5, d: 270, r: size, v: 3, bold: 0.7, fv: [size / 1.5, size / -3, size / -3]});
+    }
+}
+
+class Enemy extends Character {
+    constructor (status) {
+        super(status);
+        this.setup();
+    }
+
+    setup () {
+        this.shot = {};
+        this.bold = 0.7;
+
+        switch (this.id) {
+            case 0:
+                this.hitArea = 20;
+                this.hp = 10;
+                this.r = 15;
+
+                this.shot.interval = 30;
+                break;
+
+            case 1:
+                this.hitArea = 25;
+                this.hp = 10;
+                this.r = 20;
+
+                this.shot.interval = 67;
+                break;
+
+            case 2:
+                this.hitArea = 30;
+                this.hp = 9;
+                this.r = 18;
+
+                this.shot.interval = 36;
+                break;
+
+            case 3:
+                this.hitArea = 30;
+                this.hp = 14;
+                this.r = 20;
+
+                this.shot.interval = 80;
+                break;
+        }
+
+        this.shot.tick = this.shot.interval;
+    }
+
+    draw (context) {
+        let x = this.x;
+        let y = this.y;
+        let d = this.d || 0;
+        let r = this.r || 10;
+        let bold = this.bold || 1;
+
+        let color = '#26afff';
+        switch (this.id) {
+            case 0:
+                context.shape({x, y, d: 90 + d, r, v: 4, bold, color});
+                context.shape({x, y: y + 5, d: 90 + d, r, v: 4, bold});
+                break;
+
+            case 1:
+                context.shape({x, y, d: 270, r, v: 5, bold, color});
+                context.shape({x, y: y, d: 270, r: r + 5, v: 5, bold});
+                break;
+
+            case 2:
+                context.shape({x, y, d: 270, r: r * 1.7, v: 5, bold, color});
+                context.shape({x, y, d: 270, r: r * 1.5, v: 5, bold});
+
+                context.shape({x, y, d: 90, r: r * 0.8, v: 5, bold, color});
+                context.shape({x, y, d: 90, r, v: 5, bold});
+                break;
+
+            case 3:
+                context.shape({ x, y, d: 0, r: r * 1.2, v: 3, bold, color});
+                context.shape({ x, y, d: 0, r, v: 3, bold});
+
+                context.shape({ x, y, d: 180, r: r * 1.2, v: 3, bold, color});
+                context.shape({ x, y, d: 180, r, v: 3, bold});
+                break;
+        }
+
+        if (this.aura != undefined) {
+            context.shape({x, y, d: this.tick, r: this.aura, v: 9, bold: this.bold});
+            context.shape({x, y, d: this.tick + 45, r: this.aura, v: 9, bold: this.bold});
+        }
+
+        if (this.disappear == 1) {
+            if (this.effectTick == undefined) {
+                this.effectTick = 0;
+
+                this.effects = new Array(6).fill(null).map(_ => {
+                    let dx = -1 + (Math.random() * 2);
+                    let dy = -1 + (Math.random() * 2);
+                    return {dx, dy};
+                });
+            }
+
+            let speed = this.effectTick * 3.7;
+
+            this.effects.map(pos => {
+                let dx = pos.dx * speed;
+                let dy = pos.dy * speed;
+                context.circle({x: x + dx, y: y + dy, r, color: '#26afff'});
+                context.circle({x: x + dx, y: y + dy, r: r * 0.86, color: '#2e2e2e'});
+            });
+
+            this.effectTick ++;
+        }
+    }
+
+    update () {
+        let dx = this.dx || 0;
+        let dy = this.dy || 0;
+
+        this.move(dx, dy);
+        this.checkOverArea();
+
+        if (this.disappear == 1) {
+            this.shot.tick = Infinity;
+
+            this.bold += -this.bold / 5;
+            this.r += -this.r / 4;
+
+            if (this.r < 0.1) {
+                this.disappear = 2;
+            }
+        } else if (this.disappear == 0) {
+            this.shot.tick --;
+
+            if (this.shot.tick < 0) {
+                this.shot.tick = this.shot.interval;
+            }
+        }
+
+        this.tick ++;
+    }
+}
+
+class Bullet extends Character {
+    constructor (status) {
+        super(status);
+    }
+
+    draw (context) {
+        if (this.type == 1) {
+            context.circle({x: this.x, y: this.y, r: 3.5, bold: 0.7, color: '#26afff'});
+            context.circle({x: this.x, y: this.y, r: 1.5});
+        } else {
+            context.line({x: this.x, y: this.y + 5, x2: this.x, y2: this.y - 13, bold: 1.4, color: '#2e2e2e'});
+            context.line({x: this.x, y: this.y, x2: this.x, y2: this.y - 7, bold: 0.4});
+        }
+    }
+
+    update () {
+        this.move(this.dx, this.dy);
+
+        if (this.mdx !== undefined && Math.abs(this.dx - this.mdx) < 0.1) {
+            this.dx = this.mdx;
+        } else {
+            this.dx += this.adx || 0;
+        }
+
+        if (this.mdy !== undefined && Math.abs(this.dy - this.mdy) < 0.1) {
+            this.dy = this.mdy;
+        } else {
+            this.dy += this.ady || 0;
+        }
+
+        this.checkOverArea();
+    }
+}
